256e14c6636d6f36395aa0b4647c938e606e7fa7
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2023-03-04 14:40:56 +1300

    Clean-up sensor

--- custom_components/contact_energy/sensor.py
+++ custom_components/contact_energy/sensor.py
@@ -63,16 +63,7 @@ _LOGGER = logging.getLogger(__name__)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_EMAIL): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
-    vol.Optional(CONF_USAGE): cv.boolean,
-    vol.Optional(CONF_SOLD): cv.boolean,
-    vol.Optional(CONF_PRICES): cv.boolean,
     vol.Optional(CONF_USAGE_DAYS, default=10): cv.positive_int,
-    vol.Optional(CONF_SOLD_MEASURE, default=2): cv.positive_int,
-    vol.Optional(CONF_SOLD_DAILY, default=False): cv.boolean,
-    vol.Optional(CONF_DATE_FORMAT, default='%d %b %Y'): cv.string,
-    vol.Optional(CONF_TIME_FORMAT, default='%H:%M'): cv.string,
-    vol.Optional(CONF_SHOW_HOURLY, default=False): cv.boolean,
-    vol.Optional(CONF_HOURLY_OFFSET_DAYS, default=1): cv.positive_int,
 })
 
 SCAN_INTERVAL = timedelta(hours=3)
@@ -82,34 +73,18 @@ async def async_setup_platform(hass, config, async_add_entities,
     email = config.get(CONF_EMAIL)
     password = config.get(CONF_PASSWORD)
     
-    date_format = config.get(CONF_DATE_FORMAT)
-    time_format = config.get(CONF_TIME_FORMAT)
-    show_usage = config.get(CONF_USAGE)
-    show_sold = config.get(CONF_SOLD)
-    show_prices = config.get(CONF_PRICES)
     usage_days = config.get(CONF_USAGE_DAYS)
-    sold_measure = config.get(CONF_SOLD_MEASURE)
-    show_hourly = config.get(CONF_SHOW_HOURLY)
-    sold_daily = config.get(CONF_SOLD_DAILY)
-    hourly_offset_days = config.get(CONF_HOURLY_OFFSET_DAYS)
-    if hourly_offset_days > usage_days:
-        hourly_offset_days = usage_days
         
     api = ContactEnergyApi(email, password)
 
     _LOGGER.debug('Setting up sensor(s)...')
 
     sensors = []
-    #if show_usage:
-    sensors .append(ContactEnergyUsageSensor(SENSOR_USAGE_NAME, api, usage_days, show_hourly, hourly_offset_days, date_format, time_format))
-    #if show_sold:
-    #    sensors .append(ContactEnergySoldSensor(SENSOR_SOLD_NAME, api, sold_measure, sold_daily, date_format))
-    #if show_prices:
-    #    sensors .append(ContactEnergyPricesSensor(SENSOR_PRICES_NAME, api, date_format))
+    sensors.append(ContactEnergyUsageSensor(SENSOR_USAGE_NAME, api, usage_days))
     async_add_entities(sensors, True)
 
 class ContactEnergyUsageSensor(SensorEntity):
-    def __init__(self, name, api, usage_days, show_hourly, hourly_offset_days, date_format, time_format):
+    def __init__(self, name, api, usage_days):
         self._name = name
         self._icon = "mdi:meter-electric"
         self._state = 0
@@ -119,10 +94,6 @@ class ContactEnergyUsageSensor(SensorEntity):
         self._state_class = "total"
         self._state_attributes = {}
         self._usage_days = usage_days
-        self._show_hourly = show_hourly
-        self._date_format = date_format
-        self._time_format = time_format
-        self._hourly_offset_days = hourly_offset_days
         self._api = api
 
     @property
@@ -171,7 +142,7 @@ class ContactEnergyUsageSensor(SensorEntity):
             # Get todays date
             today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
             _LOGGER.debug('Fetching usage data')
-            data = []
+            
             kWhStatistics = []
             kWhRunningSum = 0
             
@@ -181,28 +152,25 @@ class ContactEnergyUsageSensor(SensorEntity):
             for i in range(self._usage_days):
                 previous_day = today - timedelta(days=self._usage_days - i)
                 response = self._api.get_usage(str(previous_day.year), str(previous_day.month), str(previous_day.day))
-                if response:
-                    if response[0]:
-                        for point in response:
-                            if point['value']:
-                                if point['offpeakValue'] == '0.00':
-                                    kWhRunningSum = kWhRunningSum + float(point['value'])
-                                else:
-                                    freeKWhRunningSum = freeKWhRunningSum + float(point['value'])
-                                    
-                                freeKWhStatistics.append(StatisticData(
-                                    start=datetime.strptime(point['date'], '%Y-%m-%dT%H:%M:%S.%f%z'),
-                                    sum=freeKWhRunningSum
-                                ))
-                                kWhStatistics.append(StatisticData(
-                                    start=datetime.strptime(point['date'], '%Y-%m-%dT%H:%M:%S.%f%z'),
-                                    sum=kWhRunningSum
-                                ))
-                                
-                    usage = self.make_attribute(previous_day, today, response)
-                    if usage:
-                        data.append(usage)
-            
+                if response and response[0]:
+                    for point in response:
+                        if point['value']:
+                            # If the off peak value is '0.00' then the energy is free.
+                            # HASSIO statistics requires us to add values as a sum of all previous values. 
+                            if point['offpeakValue'] == '0.00':
+                                kWhRunningSum = kWhRunningSum + float(point['value'])
+                            else:
+                                freeKWhRunningSum = freeKWhRunningSum + float(point['value'])
+                            
+                            freeKWhStatistics.append(StatisticData(
+                                start=datetime.strptime(point['date'], '%Y-%m-%dT%H:%M:%S.%f%z'),
+                                sum=freeKWhRunningSum
+                            ))
+                            kWhStatistics.append(StatisticData(
+                                start=datetime.strptime(point['date'], '%Y-%m-%dT%H:%M:%S.%f%z'),
+                                sum=kWhRunningSum
+                            ))
+
             kWhMetadata = StatisticMetaData(
                 has_mean=False,
                 has_sum=True,
@@ -222,32 +190,5 @@ class ContactEnergyUsageSensor(SensorEntity):
                 unit_of_measurement=ENERGY_KILO_WATT_HOUR
             )
             async_add_external_statistics(self.hass, freeKWHMetadata, freeKWhStatistics)
-            
-            self._state_attributes['days'] = data
         else:
             _LOGGER.error('Unable to log in')
-            
-    def make_attribute(self, date, today, response):
-        yesterday = today - timedelta(days = 1)
-        daily_usage = 0.000
-        data = {}
-        if response[0]:
-            
-            priceStatistics = []
-            for point in response:
-                if point['value'] and date:
-                    daily_usage += float(point['value']) - float(point['unchargedValue'])
-                    
-                    data['date'] = date.strftime(self._date_format)
-                    data['usage'] = daily_usage
-                    
-                    if (date == yesterday) and daily_usage != 0:
-                        self._state = data['usage']
-            else:
-                _LOGGER.debug('No usage data available for today')
-                # %1', today)
-            
-        else:
-            _LOGGER.warning('No data available for')
-            # %1', today)
-        return data
